9f66c5e4e811ec20c7a3875fa1800f6839c280a4
Olha Omelianenko <olha.omelianenko@gmail.com>
2022-03-30 14:48:27 +0300

    HW, task 2 - Lists, Generics, own collection

---
+++ Task_2_List/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2_List
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SuperHero superHero1 = new SuperHero()
+            {
+                Name = "Stephen Strange",
+                Alias = "Doctor Strange"
+            };
+            SuperHero superHero2 = new SuperHero()
+            {
+                Name = "Wanda Maximoff",
+                Alias = "Scarlet Witch"
+            };
+            SuperHero superHero3 = new SuperHero()
+            {
+                Name = "Peter Parker",
+                Alias = "Spider-Man"
+            };
+
+            SuperHeroList<SuperHero> superHeroes = new SuperHeroList<SuperHero>();
+
+            /* Add functionality */
+            superHeroes.Add(superHero1);
+            superHeroes.Add(superHero2);
+            superHeroes.Add(superHero2);
+            superHeroes.Add(superHero2);
+            superHeroes.Add(superHero3);
+
+            /* Remove functionality */
+            superHeroes.Remove(superHero2);
+            superHeroes.Remove(new SuperHero { Name = "Wanda Maximoff", Alias = "Scarlet Witch" }) ;
+            
+            /* Iterating through the collection */
+            foreach (SuperHero sh in superHeroes)
+            {
+                Console.WriteLine(sh);
+            }
+
+            /* To explicitly invoke generic methods */
+            /*Console.WriteLine();
+            IEnumerable<SuperHero> superHeroesGeneric = superHeroes;
+            foreach (SuperHero shg in superHeroesGeneric)
+            {
+                Console.WriteLine(shg);
+            }*/
+
+            /* Access by index */
+            Console.WriteLine();
+            for (int i = 0; i < superHeroes.Length; i++)
+            {
+                Console.WriteLine(superHeroes[i]);
+            }
+
+            int outOfRangeIndex = -1;
+            //int outOfRangeIndex = 3;
+            try
+            {
+                Console.WriteLine($"\n{superHeroes[outOfRangeIndex]}");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine($"Index {outOfRangeIndex} is out of range, exception!");
+            }
+        }
+    }
+}
